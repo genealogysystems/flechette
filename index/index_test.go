89ebb0d4b60352e96042046bb62dc0f0d8ca07eb
John Clark <socrates37@gmail.com>
2014-06-03 10:53:32 -0600

    first real testcase

--- index/index_test.go
+++ index/index_test.go
@@ -1,11 +1,16 @@
 package index
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestTest(t *testing.T) {
-	var idx = NewIndex()
-	fmt.Println(idx.rtree.Size())
+func TestNewIndex(t *testing.T) {
+	var (
+		idx = NewIndex()
+		size = idx.rtree.Size()
+	)
+
+	if size != 0 {
+		t.Error("Expected 0, got ", size)
+	}
 }
